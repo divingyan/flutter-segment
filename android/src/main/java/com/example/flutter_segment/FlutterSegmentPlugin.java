58280b0c7c9b59d0291475c10b5bccd946acae73
ThangNguyen91 <thangnguyenvu1991@gmail.com>
2020-11-02 11:08:14 +0700

    Update FlutterSegmentPlugin.java
    Add Adjust Destination

--- android/src/main/java/com/example/flutter_segment/FlutterSegmentPlugin.java
+++ android/src/main/java/com/example/flutter_segment/FlutterSegmentPlugin.java
@@ -16,6 +16,7 @@ import com.segment.analytics.Options;
 import com.segment.analytics.Middleware;
 import com.segment.analytics.integrations.BasePayload;
 import com.segment.analytics.android.integrations.amplitude.AmplitudeIntegration;
+import com.segment.analytics.android.integrations.adjust.AdjustIntegration;
 import static com.segment.analytics.Analytics.LogLevel;
 
 import java.util.LinkedHashMap;
